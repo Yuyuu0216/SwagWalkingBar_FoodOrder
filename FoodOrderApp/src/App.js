5b445242ad12701800467512b47bd1adda7d27d2
Yuyuu0216 <zoe106429@gmail.com>
2024-05-29 01:52:59 +0800

    start To Login

--- FoodOrderApp/src/App.js
+++ FoodOrderApp/src/App.js
@@ -3,9 +3,20 @@ import Header from "./components/Layout/Header";
 import Foods from "./components/Foods/Foods";
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
+import LoginPage from "./components/Login/LoginPage";
 
 function App() {
+  const [nowSelected,setNowSelected] = useState('');
   const [cartIsShown,setCartIsShown] = useState(false);
+  const [loginIsShown,setLoginIsShown] = useState(false);
+
+  const whereToGo = (e) => {
+    if(e == '1'){
+      showCartHandler()
+    }else if(e == '2'){
+
+    }
+  }
 
   const showCartHandler = () => {
     setCartIsShown(true);
@@ -15,11 +26,20 @@ function App() {
     setCartIsShown(false);
   }
 
+  const hideLoginHandler = () => {
+    setLoginIsShown(false);
+  }
+
+  const showLoginHandler = () => {
+    setLoginIsShown(true);
+  }
+
   return (
     <CartProvider>
       {/* 如果cartIsShown是true則顯示購物車，反之則否 */}
       {cartIsShown && <Cart closing={hideCartHandler} onClose={hideCartHandler}/>}
-      <Header clicking={showCartHandler}/>
+      {loginIsShown && <LoginPage closing={showLoginHandler} onClose={hideLoginHandler}/>}
+      <Header clickingCart={showCartHandler} clickingLogin={whereToGo}/>
       <main>
       <Foods />
       </main>
